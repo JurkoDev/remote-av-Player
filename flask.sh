b9a94cd30f16f8d0c256c47c028b2ba9b5960fb6
hribik100 <50539591+hribik100@users.noreply.github.com>
2022-05-03 15:41:37 +0200

    rpi script update

--- flask.sh
+++ flask.sh
@@ -1,2 +1,3 @@
+cd /home/pi/Desktop/remote-av-Player
 . venv/bin/activate
 flask run --host=0.0.0.0
--- ws.sh
+++ ws.sh
@@ -1,4 +1,3 @@
 cd /home/pi/Desktop/remote-av-Player
 git pull
-sh /home/pi/Desktop/remote-av-Player/flask.sh &&
 python /home/pi/Desktop/test.py
