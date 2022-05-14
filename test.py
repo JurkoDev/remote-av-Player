95dfd8faecf0c80bda4a0721b57156ae3a2c75cd
hribik100 <50539591+hribik100@users.noreply.github.com>
2022-05-14 10:36:35 +0200

    update

--- test.py
+++ test.py
@@ -10,6 +10,16 @@ async def echo(websocket):
     async for message in websocket:
         websockets.broadcast(CLIENTS, message)
         temp = json.loads(message)
+        if temp["command"] == "media_youtube_offline":
+            link = temp["link"]
+            stream = os.popen('cd C:\\Users\\jurko\Desktop\\firebase\\public && youtube-dl --no-call-home -o "%(id)s.mp4" ' + link)
+            streamtemp = stream.read()
+            print("dowload done")
+            stream = os.popen('youtube-dl --no-call-home --get-filename -o "%(id)s.mp4" ' + link)
+            streamtemp = stream.read()
+            jsontemp = json.loads('{"command":"media_youtube_video_offline","url":""}')
+            jsontemp["url"] = "http://localhost:5000/" + streamtemp
+            websockets.broadcast(CLIENTS, json.dumps(jsontemp))
         if temp["command"] == "media_play_youtube":
             link = temp["link"]
             stream = os.popen("youtube-dl -f 136 -g " + link + "--no-call-home")
